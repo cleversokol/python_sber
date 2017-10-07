3f68ab11b6735974410abc0d19ba9cfdff6ceda4
Sergey Sokolov <cleversokol@gmail.com>
2017-10-08 00:54:09 +0300

    Cleaned up 2_2_9

--- module2/2_2_9.py
+++ module2/2_2_9.py
@@ -1,15 +1,11 @@
-#from simplecrypt import decrypt, DecryptionException
-#import simplecrypt
-from simplecrypt import *
+from simplecrypt import decrypt, DecryptionException
 
 with open("encrypted.bin", "rb") as inp:
     encrypted = inp.read()
     print(encrypted)
     with open("passwords.txt", "r") as pwd:
         passwords = pwd.read().split("\n")
-        #passwords = pwd.readlines()
         for password in passwords:
-            #password.pop()
             print("password = ",password)
             try:
                 answer = decrypt(password,encrypted)

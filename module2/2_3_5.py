c2b4f652a01c11fe58cc1a10c43d070adff71ea7
Sokolow-SS <SSSokolow@sberbank.ru>
2017-10-09 14:34:50 +0300

    Completed 2.3

--- module2/2_3_5.py
+++ module2/2_3_5.py
@@ -1,18 +1,15 @@
+import itertools
+
 def primes():
-    number = 2
-#    print("BEFORE while")
+    number = 1
     while True:
-#        print("IN while")
         divisors = 0
+        number += 1
         for i in range(1, number+1):
             if number % i == 0:
                 divisors += 1
-#        print("number = " + number)
-#        print("divisors" + divisors)
         if divisors < 3 :
             yield number
-        else:
-            number += 1
 
-for j in range (30):
-    print(primes())
+print(list(itertools.takewhile(lambda x : x <= 31, primes())))
+# [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]

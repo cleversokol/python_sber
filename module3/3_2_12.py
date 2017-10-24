951dddc6411d4d7ace3ab72b1487bbe6ba45a878
Sokolow-SS <SSSokolow@sberbank.ru>
2017-10-24 18:49:26 +0300

    Completed 3_2_12

---
+++ module3/3_2_12.py
@@ -0,0 +1,19 @@
+import re
+import sys
+
+pattern = re.compile(r'human')
+
+for line in sys.stdin:
+    line = line.rstrip()
+    if re.search(pattern, line) != None:
+        line = re.sub(pattern, "computer", line)
+        print(line)
+
+#string = "I need to understand the human mind"
+#string = "humanity"
+#
+#if re.search(pattern, string) != None:
+#    string = re.sub(pattern, "computer", string)
+#    print(string)
+#else:
+#    print("no")
